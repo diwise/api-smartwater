6abde3aa254d453f21a990dd080a7e85230ac13e
shestyf <sofia.styf@masarinconsulting.se>
2022-01-07 15:04:01 +0100

    added mock for database, fixed new unit test for application

--- internal/pkg/application/application_test.go
+++ internal/pkg/application/application_test.go
@@ -1,1 +1,30 @@
 package application
+
+import (
+	"testing"
+	"time"
+
+	"github.com/matryer/is"
+	"github.com/rs/zerolog/log"
+	"github.com/sundsvall/api-smartwater/internal/pkg/infrastructure/repositories/database"
+	"github.com/sundsvall/api-smartwater/internal/pkg/infrastructure/repositories/models"
+)
+
+func newAppForTesting() (*database.DatastoreMock, Application) {
+	db := &database.DatastoreMock{
+		StoreWaterConsumptionFunc: func(device string, consumption float64, timestamp time.Time) (*models.WaterConsumption, error) {
+			return nil, nil
+		},
+	}
+	log := log.Logger
+
+	return db, newWaterConsumptionApp(db, log, "serviceName")
+}
+func TestUpdateWaterConsumption(t *testing.T) {
+	is := is.New(t)
+	db, app := newAppForTesting()
+
+	err := app.UpdateWaterConsumption("device", 172.0, time.Now().UTC())
+	is.NoErr(err)                                     // Check error
+	is.Equal(len(db.StoreWaterConsumptionCalls()), 1) // StoreWaterConsumption should have been called once
+}
